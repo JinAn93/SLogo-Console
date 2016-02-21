dfa4bb3c41fc1ca83fa92db9d713fda570a827bb
Jin An <sa190@duke.edu>
2016-02-21 18:46:31 -0500

    Turtle and CommandDecoder Class

---
+++ src/CommandDecoder.java
@@ -0,0 +1,15 @@
+/**
+ * CommandDecoder serves to parse the input and check whether there exist any error.
+ * 
+ * @author Jin An
+ *
+ */
+public class CommandDecoder {
+    public boolean checkError (String input) {
+        return false;
+    }
+
+    public void parseCommand (String input) {
+
+    }
+}
