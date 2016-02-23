3cba2414aab6db76a866e9925d60c67893074eeb
David Yan <davidyanduke@gmail.com>
2016-02-23 00:13:10 -0500

    Basic GUI elements

---
+++ src/frontend/Main.java
@@ -0,0 +1,11 @@
+package frontend;
+import frontend.UIFrame;
+import java.io.IOException;
+/**
+ * @author davidyan
+ */
+public class Main {	
+	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		UIFrame Main = UIFrame.getInstance();
+	}
+}
