28273c0b4e973adbb38b0ae27e925c894cc61a8a
David Yan <davidyanduke@gmail.com>
2016-02-25 12:55:45 -0500

    Update FxMain

--- src/fxFrontend/FxMain.java
+++ src/fxFrontend/FxMain.java
@@ -1,21 +1,5 @@
 package fxFrontend;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class FxMain extends Application {
