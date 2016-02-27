aa0445fc97595cfc602a3a9cdf5c7f6726e0cdbe
Jin An <sa190@duke.edu>
2016-02-27 14:26:23 -0500

    TurtleQuery in Output

--- src/Main/Output.java
+++ src/Main/Output.java
@@ -1,13 +1,22 @@
 package Main;
 
 public class Output {
-	private String myOutputCommand;
-	
-	public Output(String command){
-		myOutputCommand = command; 
-	}
-	
-	public String getCommand(){
-		return myOutputCommand; 
-	}
+    private String myOutputCommand;
+    private Turtle myTurtle;
+
+    public Output (String command) {
+        myOutputCommand = command;
+    }
+
+    public String getCommand () {
+        return myOutputCommand;
+    }
+
+    public void setCommand (String command) {
+        this.myOutputCommand = command;
+    }
+    
+    public void updateTurtle (Turtle turtle){
+        this.myTurtle = turtle;
+    }
 }
