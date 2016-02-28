5e558b495c0d86a0f6c4dbb0e265325e79f94196
Jin An <sa190@duke.edu>
2016-02-28 02:41:45 -0500

    Forward

--- src/TurtleCommand/Forward.java
+++ src/TurtleCommand/Forward.java
@@ -1,9 +1,27 @@
 package TurtleCommand;
 
-import java.util.List;
-import Tree.TreeNode;
+import Commands.Node;
 import Commands.TurtleCommand;
+import Main.Turtle;
 
-public class Forward extends TurtleCommand{
-    
+
+public class Forward extends TurtleCommand {
+    public final int NUMBEROFCHILDREN = 1;
+    private Node[] children = new Node[NUMBEROFCHILDREN];
+
+    @Override
+    public String executeCommand (Turtle turtle) {
+        return String.valueOf((turtle.getXCor()));
+    }
+
+    public String getValue (Turtle turtle) {
+        return executeCommand(turtle);
+    }
+
+    @Override
+    public void setChildren (Node[] kids) {
+        for (int i = 0; i < kids.length; i++) {
+            children[i] = kids[i];
+        }
+    }
 }
