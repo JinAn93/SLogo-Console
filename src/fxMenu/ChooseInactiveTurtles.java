7f3ac954fa348580d044afe468ef625653cdda57
Cody <codyli332@gmail.com>
2016-03-10 18:15:30 -0500

    Refactor chooseinactiveturtle

--- src/fxMenu/ChooseInactiveTurtles.java
+++ src/fxMenu/ChooseInactiveTurtles.java
@@ -23,15 +23,19 @@ public class ChooseInactiveTurtles {
 	private List<SingleTurtle> myTurtleList;
 	private Menu myTurtleInactiveMenu;
 	private MenuItem myChoose;
-
+	private Button myButton; 
+	private TextField myInput;
+	private ArrayList<Integer> myIndices; 
+	private String myCommand; 
+	
 	public ChooseInactiveTurtles(List<Integer> mynumlist, List<SingleTurtle> myturts){
 		myTurtleInactiveMenu = new Menu("Choose Inactive Turtles");
 		myTurtleList = myturts;
 		myInactiveList = mynumlist;
 		getOptions();
-		myTurtleInactiveMenu.getItems().add(myChoose);
-		
+		myTurtleInactiveMenu.getItems().add(myChoose);	
 	}
+	
 	private void getOptions(){
 		myChoose = new MenuItem("Choose the inactive turtles");
         myChoose.setOnAction(new EventHandler<ActionEvent>() {
@@ -40,54 +44,63 @@ public class ChooseInactiveTurtles {
             	Pane root = new Pane();
             	Scene myScene = new Scene(root,300,200);
             	HBox myBox = new HBox();
-            	TextField myInput = new TextField();
-            	Button myButton = new Button("Submit");
+            	myInput = new TextField();
+                myButton = new Button("Submit");
             	myBox.getChildren().addAll(myInput,myButton);
             	root.getChildren().add(myBox);
             	myTempStage.setScene(myScene);
             	myTempStage.show();
-            	// inactive 1,2,3
-        		myButton.setOnAction(new EventHandler<ActionEvent>(){
-        			public void handle(ActionEvent event) {
-        				String myInputCommand = myInput.getText();
-        				String[] mySplit = myInputCommand.split(" ");
-        				String myCommand = mySplit[0];
-        				String[] myIndicesString = mySplit[1].split(",");
-        				ArrayList<Integer> myIndices = new ArrayList<Integer>();
-        				for(String astring: myIndicesString){
-        					myIndices.add(Integer.parseInt(astring));
-        				}
-        				if(myCommand.equals("inactive")){
-        					for(Integer anint: myIndices){
-        						if(!myInactiveList.contains(anint)){
-        							myInactiveList.add(anint);
-        						}
-        					}
-        				}else if(myCommand.equals("active")){
-        					for(Integer anint: new ArrayList<Integer>(myIndices)){
-        						if(myInactiveList.contains(anint)){
-        							myInactiveList.remove(anint);
-        						}
-        					}
-        					
-        				}
-        				for(int i=0; i<myTurtleList.size(); i++){
-        					if(myInactiveList.contains(i)){
-        					myTurtleList.get(i).setActive(false);
-        					}else{
-        						myTurtleList.get(i).setActive(true);
-        					}
-        				}
-        				
-        				
-        				
-        			}
-        			});
-
+            	pickTurtle();
             }
         });
 
 	}
+	
+    private void pickTurtle() {
+    	myButton.setOnAction(new EventHandler<ActionEvent>(){
+    		public void handle(ActionEvent event) {
+    			String myInputCommand = myInput.getText();
+    			String[] mySplit = myInputCommand.split(" ");
+    			myCommand = mySplit[0];
+    			String[] myIndicesString = mySplit[1].split(",");
+    			myIndices = new ArrayList<Integer>();
+    			for(String astring: myIndicesString){
+    				myIndices.add(Integer.parseInt(astring));}
+    			commandCheck();
+    			for(int i=0; i<myTurtleList.size(); i++){
+    				if(myInactiveList.contains(i)){
+    					myTurtleList.get(i).setActive(false);
+    				}else{
+    					myTurtleList.get(i).setActive(true);}
+    			}
+    		}
+    	});
+    }
+    
+    private void commandCheck() {
+		if(myCommand.equals("inactive")){
+			iterateInactive();
+		}else if(myCommand.equals("active")){
+			iterateActive();
+		}
+	}
+
+    private void iterateActive() {
+		for(Integer anint: new ArrayList<Integer>(myIndices)){
+			if(myInactiveList.contains(anint)){
+				myInactiveList.remove(anint);
+			}
+		}
+	}
+
+	private void iterateInactive() {
+		for(Integer anint: myIndices){
+			if(!myInactiveList.contains(anint)){
+				myInactiveList.add(anint);
+			}
+		}
+	}
+    
 	public Menu getMenu(){
 		return myTurtleInactiveMenu;
 	}
