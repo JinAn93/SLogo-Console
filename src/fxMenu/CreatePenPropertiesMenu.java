1fc34968553ba76c7f0dd158415719dc85802d9a
Cody <codyli332@gmail.com>
2016-03-10 18:19:23 -0500

    Refactor pen properties menu

--- src/fxMenu/CreatePenPropertiesMenu.java
+++ src/fxMenu/CreatePenPropertiesMenu.java
@@ -16,58 +16,66 @@ import javafx.scene.paint.Color;
 public class CreatePenPropertiesMenu {
     private List<SingleTurtle> myTurtle;
     private Menu myPenUpMenu;
+    private MenuItem setPenDown, setPenUp, setPenWidth, setDashedLines;
 
     public CreatePenPropertiesMenu (List<SingleTurtle> turt) {
         myTurtle = turt;
         myPenUpMenu = new Menu("Pen Properties");
-        MenuItem setPenDown = new MenuItem("Set Pen Down");
-        MenuItem setPenUp = new MenuItem("Set Pen Up");
-        MenuItem setPenWidth = new MenuItem("Set Pen Width");
-        MenuItem setDashedLines = new MenuItem("Enable Dashed Lines");
-
-        setDashedLines.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle (ActionEvent e) {
-                for (Turtle aturtle : myTurtle)
-                    aturtle.setPenDashed();
-            }
-        });
-        
-        setPenDown.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle (ActionEvent e) {
-                for (Turtle aturtle : myTurtle)
-                    aturtle.setPen(1);
-            }
-        });
-
-        setPenUp.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle (ActionEvent e) {
-                for (Turtle aturtle : myTurtle)
-                    aturtle.setPen(0);
-            }
-        });
-
-        setPenWidth.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle (ActionEvent e) {
-            	
-            	TextInputDialog dialog = new TextInputDialog("");
-            	dialog.setTitle("Pen Width");
-            	dialog.setHeaderText("Choose Pen Width");
-            	dialog.setContentText("Please choose a pen width:");
-
-            	Optional<String> result = dialog.showAndWait();
-            	int useResult = Integer.parseInt(result.get());
-            	
-
-                for (Turtle aturtle : myTurtle)
-                    aturtle.setPenWidth(useResult);
-            }
-        });
-
+        setPenDown = new MenuItem("Set Pen Down");
+        setPenUp = new MenuItem("Set Pen Up");
+        setPenWidth = new MenuItem("Set Pen Width");
+        setDashedLines = new MenuItem("Enable Dashed Lines");
+        dashLineMenu();   
+        penDownMenu();
+        penUpMenu();
+        penWidthMenu();
         myPenUpMenu.getItems().add(setPenDown);
         myPenUpMenu.getItems().add(setPenUp);
         myPenUpMenu.getItems().add(setPenWidth);
         myPenUpMenu.getItems().add(setDashedLines);
     }
+
+	private void penWidthMenu() {
+		setPenWidth.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle (ActionEvent e) {
+            	TextInputDialog dialog = new TextInputDialog("");
+            	dialog.setTitle("Pen Width");
+            	dialog.setHeaderText("Choose Pen Width");
+            	dialog.setContentText("Please choose a pen width:");
+            	Optional<String> result = dialog.showAndWait();
+            	int useResult = Integer.parseInt(result.get());
+                for (Turtle aturtle : myTurtle)
+                    aturtle.setPenWidth(useResult);
+            }
+        });
+	}
+
+	private void penUpMenu() {
+		setPenUp.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle (ActionEvent e) {
+                for (Turtle aturtle : myTurtle)
+                    aturtle.setPen(0);
+            }
+        });
+	}
+
+	private void penDownMenu() {
+		setPenDown.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle (ActionEvent e) {
+                for (Turtle aturtle : myTurtle)
+                    aturtle.setPen(1);
+            }
+        });
+	}
+
+	private void dashLineMenu() {
+		setDashedLines.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle (ActionEvent e) {
+                for (Turtle aturtle : myTurtle)
+                    aturtle.setPenDashed();
+            }
+        });
+	}
 
     public Menu getPenUpMenu () {
         return myPenUpMenu;
